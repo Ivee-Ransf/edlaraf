4a2b2b4c5345427e33837f4cf5209bd104ac6516
Ivee Ransford <iveeransf@gmail.com>
2018-12-23 09:40:47 -0600

    added favicon

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,22 @@
-import React from 'react'
-
-import Layout from '../components/layout'
-import SEO from '../components/seo'
+import React from "react";
+import { Link } from "gatsby";
+import FNF from "../components/fnf-image";
+import Layout from "../components/layout";
+import SEO from "../components/seo";
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
     <h1>PAGE NOT FOUND</h1>
-    <p>Looks like you strayed away from the herd.  Here's a friend to help you find your way back.</p>
+    <p>
+      Looks like you strayed away from the herd. Here's a friend to help you
+      find your way back.
+    </p>
+    <div style={{ maxWidth: "750px", marginBottom: "1.45rem" }}>
+      <FNF />
+    </div>
+    <Link to="/">Return to the herd</Link>
   </Layout>
-)
+);
 
-export default NotFoundPage
+export default NotFoundPage;
